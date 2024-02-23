494dfb30b64970d14652e23f96cfdd77000e809e
Luis Costa <lipemenezescosta@gmail.com>
2024-02-23 14:39:12 -0300

    Semana 07 - flask e Calculadora.

---
+++ Semana07/flask/meusite.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from random import randint
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    variavel = "Game: Advinhe o número correto"
+
+    if request.method == "GET":
+        return render_template("index.html", variavel=variavel)
+    else:
+        numero = randint(1,20)
+        palpite = int(request.form.get("name"))
+
+        if numero == palpite:
+            return '<h1> Resultado: Você ganhou</h1>'
+        else:
+            return '<h1> Resultado: Você perdeu</h1>'
+
+@app.route('/<string:nome>')
+def error(nome):
+    variavel = f'Página ({nome}) não existe'
+    return render_template("error.html", variavel=variavel)
